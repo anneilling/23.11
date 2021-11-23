568122fdb375ced90377d12e4cb2cf31bdd489ae
Opilane TTHK <opilane@tthk.ee>
2021-11-23 13:59:58 +0200

    Add project files.

---
+++ _23.11.py
@@ -0,0 +1,30 @@
+﻿from module1 import *
+users=["Anne"]
+psword=["12345"]
+
+while True:
+    print("Näita kõike-0,\nReg-1,\nAvt-2,\nVälja-3")
+    v=int(input())
+    if v==0:
+        koik_kasutajad(users,psword)
+    elif v==1:
+        print("Registreerimine ")
+        while 1:
+            log=input("Login ")
+            if log not in users:
+                print("Login soobib")
+                break
+            else:
+                 print("See nimi juba on olemas listis")
+        v=input("Arvuti-A või ise-I loob parool ")
+        if v.upper()=="A":
+            pas=passautomat()
+        elif v.upper()=="I":
+            while 1:
+                pas=input("Sisesta oma parool ")
+                tulemus=passcontrol(pas)
+                if tulemus==True:
+                    users.append(log)
+                    psword.append(pas)
+                    break
+        
---
+++ module1.py
@@ -0,0 +1,40 @@
+﻿#funktsioonide loomine
+from random import *
+def passcontrol(psword):
+    ls=list(psword)
+    for i in psword:
+        if i.isdigit()== True:
+            digit="True"
+        if i.isalpha()== True:
+            alpha="True"
+        if i.isupper()==True:
+            upper="True"
+        if i.islower()==True:
+            lower="True"
+        if i in list('.','-','+','/','%'):
+            symbol="True"
+    if digit=="True" and upper=="True" and alpha=="True" and lower=="True" and symbol=="True":
+        ans=True
+    else:
+        ans=False
+    return ans
+def passautomat()-> str:
+ #parool genereeritakse masinaga
+    str0=".,:;!_*-+()/#¤%&"
+    str1="0123456789"
+    str2="qwertyuiopasdfghjklzxcvbnm"
+    str3=str2.upper() #QWERTYUIOPASDFGHJKLZXCVBNM
+    str4=str0+str1+str2+str3
+    ls=list(str4) #list
+    random.shuffle(ls) #segage väärtusi
+#eraldame loendist 12 suvalist väärtust
+    psword="".join([choice(ls) for x in range(12)])
+#parool on valmis
+    return psword
+
+def koik_kasutajad(users,passwords):
+    i=0
+    for user in users:
+        print(user,end="-")
+        print(psword[i])
+        i+=1
